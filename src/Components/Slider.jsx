39c100609f7febb7390e0d1390b9bf9f296725d5
Kris Kurzawa <kurzawa1@yahoo.com>
2023-11-19 13:38:56 -0500

    added images and videos to folder

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import GlobalApi from "../Services/GlobalApi";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 const IMAGE_BASE_URL = "https://www.themoviedb.org/t/p/original"
+const screenWidth = window.innerWidth;
 
 function Slider() {
     const [movieList, setMovieList] = useState([]);
+    const elementRef = useRef();
     useEffect(() => {
         getTrendingMovies();
     }, [])
@@ -16,14 +18,26 @@ function Slider() {
             setMovieList(resp.data.results);
         })
     }
+
+    const sliderRight = (element) => {
+        element.scrollLeft += screenWidth - 110
+    }
+    const sliderLeft = (element) => {
+        element.scrollLeft += screenWidth - 110
+    }
+
     return (
         <div>
-            <HiChevronLeft className="text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer" />
-            <HiChevronRight className="text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0" />
-            <div className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none ">
+            <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
+                onClick={() => sliderLeft(elementRef.current)} />
+            <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
+                onClick={() => sliderRight(elementRef.current)} />
+
+            <div className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth" ref={elementRef}>
                 {movieList.map((item) => {
                     return <img src={IMAGE_BASE_URL + item.backdrop_path}
-                        className="min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md" />
+                        className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] 
+                        border-gray-400 transition-all duration-100 ease-in" />
                 })}
             </div>
         </div>
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-// const movieBaseUrl = 'https://api.themoviedb.org/3';
-// const api_key = '8a597021b72b44bff367b21fee9c9024';
-
 const getTrendingVideos = axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=8a597021b72b44bff367b21fee9c9024');
 
 export default {
