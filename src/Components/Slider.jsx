34ea2f592e04cbe3be0bfd9063d6db9d56915139
Kris Kurzawa <kurzawa1@yahoo.com>
2023-11-19 20:44:16 -0500

    bug fix in slider.jsx

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -19,10 +19,10 @@ function Slider() {
     }
 
     const sliderRight = (element) => {
-        element.scrollLeft -= screenWidth - 110
+        element.scrollLeft += screenWidth - 110
     }
     const sliderLeft = (element) => {
-        element.scrollLeft += screenWidth - 110
+        element.scrollLeft -= screenWidth - 110
     }
 
     return (
